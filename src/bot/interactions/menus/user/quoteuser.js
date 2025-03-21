04939d659ab61edaf01104df84bdedc4ca39841b
aptfury <blake.lemarr@gmail.com>
2025-03-21 00:32:23 -0400

    created quote user context menu option with a modal for inputting quote

--- src/bot/interactions/menus/user/quoteuser.js
+++ src/bot/interactions/menus/user/quoteuser.js
@@ -1,32 +1,40 @@
 require('dotenv').config({ path: '../src/configs/.env' });
 const { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder, MessageFlags } = require('discord.js');
+const { customQuoteModal } = require('../../modals/addUserQuote');
 const quotesChannel = process.env.dandy_quotes_chan_id;
 
 module.exports = {
     data: new ContextMenuCommandBuilder()
-        .setName('Quote Message')
+        .setName('Quote User')
         .setType(ApplicationCommandType.User),
     async execute(interaction) {
         const targetUser = interaction.targetUser;
-        const channel = await interaction.guild.channels.cache.get(quotesChannel);
+        const id = targetUser.id;
+        const member = interaction.guild.members.cache.get(id);
+        const nick = member.nickname || member.displayName;
+        const channel = interaction.guild.channels.cache.get(quotesChannel);
 
         if (targetUser === interaction.client.user) {
             interaction.reply({ content: "Sorry! I am simply too powerful to be quoted!", flags: MessageFlags.Ephemeral });
             return;
         }
 
-        const quoteEmbed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setDescription(`${content}`)
-            .setTimestamp()
-            .setFooter({ text: `${author}` });
+        customQuoteModal(interaction, nick);
 
-        try {
-            await channel.send({ embeds: [ quoteEmbed ] });
-            await interaction.reply({ content: "The message you selected has been sent to the quotes channel!", flags: MessageFlags.Ephemeral });
-        }
-        catch (e) {
-            throw new Error(e);
-        }
+        const filter = (interaction) => interaction.customId === 'addUserQuote';
+        interaction.awaitModalSubmit({ filter, time: 15_000 })
+        .then((interaction) => {
+            const response = interaction.fields.getTextInputValue('content');
+
+            const quoteEmbed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setDescription(`${response}`)
+                .setTimestamp()
+                .setFooter({ text: `${nick}` });
+            
+            channel.send({ embeds: [ quoteEmbed ] });
+            interaction.reply({ content: "Sent to the quote channel!", flags: MessageFlags.Ephemeral });
+        })
+        .catch(e => console.log(e));
     }
 }
