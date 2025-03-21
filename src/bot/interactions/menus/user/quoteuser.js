591fdbda1a0cf22f988e560b41360e9d2b74cfed
aptfury <blake.lemarr@gmail.com>
2025-03-21 08:38:05 -0400

    identified bug coming from comma in user name

--- src/bot/interactions/menus/user/quoteuser.js
+++ src/bot/interactions/menus/user/quoteuser.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: '../src/configs/.env' });
-const { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder, MessageFlags, UserContextMenuCommandInteraction, ModalSubmitInteraction } = require('discord.js');
+const { ContextMenuCommandBuilder, ApplicationCommandType, EmbedBuilder, MessageFlags } = require('discord.js');
 const { customQuoteModal } = require('../../modals/addUserQuote');
 const quotesChannel = process.env.dandy_quotes_chan_id;
 
@@ -7,14 +7,13 @@ module.exports = {
     data: new ContextMenuCommandBuilder()
         .setName('Quote User')
         .setType(ApplicationCommandType.User),
-    /**
-     * 
-     * @param {UserContextMenuCommandInteraction} interaction 
-     * @returns 
-     */
     async execute(interaction) {
-        const targetMember = interaction.targetMember;
-        const nick = targetMember.nickname || targetMember.displayName;
+        /* const targetMember = interaction.targetMember;
+        const nick = targetMember.nickname || targetMember.displayName; */
+        const targetUser = interaction.targetUser;
+        const id = targetUser.id;
+        const member = interaction.guild.members.cache.get(id);
+        const nick = member.nickname;
         const channel = interaction.guild.channels.cache.get(quotesChannel);
 
         if (targetUser === interaction.client.user) {
@@ -22,22 +21,20 @@ module.exports = {
             return;
         }
 
+        /**
+         * FIXME:
+         *      - Is no longer working after messing with type enforcers and event handler set ups
+         *      - MessageQuote is working fine so it may be either the function or an issue in the base code here.
+         *      - Causing bugs severe enough to break the code. This should be handled ASAP.
+         *      - Appears to be an issue with the modal so it might be worth rebuilding the Modal from the ground up.
+         *      - The modal isn't populating on the discord side at all but is a giving an error that it did not receive a string for the response.
+         *      - will post error in bot-dev
+         */
         customQuoteModal(interaction, nick);
 
-        /**
-         * 
-         * @param {ModalSubmitInteraction} interaction 
-         * @returns 
-         */
         const filter = (interaction) => interaction.customId === 'addUserQuote';
         interaction.awaitModalSubmit({ filter, time: 15_000 })
-        .then(
-            /**
-             * 
-             * @param {ModalSubmitInteraction} interaction 
-             * @returns
-             */
-            (interaction) => {
+        .then((interaction) => {
                 const response = interaction.fields.getTextInputValue('content');
 
                 const quoteEmbed = new EmbedBuilder()
@@ -48,8 +45,7 @@ module.exports = {
                 
                 channel.send({ embeds: [ quoteEmbed ] });
                 interaction.reply({ content: "Sent to the quote channel!", flags: MessageFlags.Ephemeral });
-            }
-        )
+        })
         .catch(e => console.log(e));
     }
 }
