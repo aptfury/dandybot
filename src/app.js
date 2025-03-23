739bbc0af2cd810d5be79150fc1284ecbfefcb9f
aptfury <blake.lemarr@gmail.com>
2025-03-23 12:31:41 -0400

    added command handler

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import './configs/env.js';
 import logger from './configs/logger.js';
+import fs from 'node:fs';
+import path from 'node:path';
 
 const token = process.env.TOKEN;
 
@@ -29,6 +31,32 @@ const bot = new Client({
         GatewayIntentBits.MessageContent
     ]
 });
+
+// bot command handler
+bot.commands = new Collection();
+
+const commandsPath = path.join(__dirname, './bot/interactions/commands');
+const commandFolders = fs.readdirSync(commandsPath);
+
+for (const folder of commandFolders) {
+    const commandPath = path.join(commandsPath, folder);
+    const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = path.join(commandPath, file);
+        import(filePath)
+        .then(command => {
+            if ('data' in command && 'execute' in command) {
+                bot.commands.set(command.data.name, command);
+                logger.info(`${command.data.name} has been added.`);
+            }
+            else {
+                logger.warn(`The command at ${filePath} is missing the "data" and/or "execute" property.`);
+            }
+        })
+        .catch(e => logger.error(e));
+    }
+}
 
 bot.once(Events.ClientReady, readyBot => {
     logger.info(`Ready and logged in as ${readyBot.user.tag}`);
