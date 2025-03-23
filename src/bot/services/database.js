31083133af34e313c629b7bfc2098ed6f6b2c33e
aptfury <blake.lemarr@gmail.com>
2025-03-23 15:31:46 -0400

    confirmed working database and created timeout

--- src/bot/services/database.js
+++ src/bot/services/database.js
@@ -1,30 +1,19 @@
 /**
  *  Imports
  */
-import '../../configs/env.js';
-import { MongoClient } from 'mongodb';
-import { logger } from '../../configs/logger.js';
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const { UUID } = require('bson');
 
 // Connection String
 const uri = process.env.CONNECTION_STRING;
 
 // Database Instance
-const con = new MongoClient(uri);
+const con = new MongoClient(uri, {
+    pkFactory: {
+        createPk: () => new UUID().toBinary()
+    },
+    connectTimeoutMS: 10000,
+});
 
-// Run Database
-async function run() {
-    try {
-        const db = con.db('dand_e');
-        const toons = db.collection('toons');
-
-        // Test query
-        const query = { name: 'Boxten' };
-        const toon = await toons.findOne(query);
-
-        console.log(toon);
-    } finally {
-        // Ensure that client closes when you finish or error
-        await con.close();
-    }
-}
-run().catch(console.dir);
+module.exports = { con };
